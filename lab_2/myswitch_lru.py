51902cd6e25ce402f6482156865a6a5f0100f1ba
181860109 <181860109@smail.nju.edu.cn>
2020-03-11 21:55:57 +0800

    finish the three way test file be left

--- lab_2/myswitch_lru.py
+++ lab_2/myswitch_lru.py
@@ -6,11 +6,11 @@ in it, not a learning switch.  (I.e., it's currently a switch
 that doesn't learn.)
 '''
 from switchyard.lib.userlib import *
-
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
-
+    forward_table=list()
+    max_len=5
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
@@ -18,13 +18,34 @@ def main(net):
             continue
         except Shutdown:
             return
-
         log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
         if packet[0].dst in mymacs:
-            log_debug ("Packet intended for me")
+            log_debug("Packet intended for me")
         else:
-            for intf in my_interfaces:
-                if input_port != intf.name:
-                    log_debug ("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            src_mac=str(packet[Ethernet].src)
+            dst_mac=str(packet[Ethernet].dst)
+            log_info('from {} to {}'.format(src_mac,dst_mac))
+            src_flag,dst_flag=False,False 
+            for i in range(len(forward_table)):
+                if forward_table[i][0]==src_mac:
+                    forward_table[i][1]=input_port
+                    src_flag=True
+                    break
+            if src_flag==False:
+                if len(forward_table)==max_len:
+                    forward_table.pop()
+                forward_table.insert(0,[src_mac,input_port])
+            for i in range(len(forward_table)):
+                if forward_table[i][0]==dst_mac:
+                    dst_pair=forward_table[i]
+                    forward_table.remove(forward_table[i])
+                    forward_table.insert(0,dst_pair)
+                    net.send_packet(dst_pair[1],packet)
+                    dst_flag=True
+                    break
+            if dst_flag==False:
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        log_debug ("Flooding packet {} to {}".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)
     net.shutdown()
--- lab_2/myswitch_to.py
+++ lab_2/myswitch_to.py
@@ -6,11 +6,12 @@ in it, not a learning switch.  (I.e., it's currently a switch
 that doesn't learn.)
 '''
 from switchyard.lib.userlib import *
+import time
 
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
-
+    forward_table=dict()
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
@@ -18,13 +19,25 @@ def main(net):
             continue
         except Shutdown:
             return
-
+        now_time=time.time()
+        for key,value in list(forward_table.items()):
+            if now_time-value[1]>=10.0:
+                log_info("del {}:{}".format(key,value))
+                forward_table.pop(key)
         log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
         if packet[0].dst in mymacs:
-            log_debug ("Packet intended for me")
+            log_debug("Packet intended for me")
         else:
-            for intf in my_interfaces:
-                if input_port != intf.name:
-                    log_debug ("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            src_mac=str(packet[Ethernet].src)
+            dst_mac=str(packet[Ethernet].dst)
+            log_info('from {} to {}'.format(src_mac,dst_mac))
+            forward_table[src_mac]=(input_port,now_time)
+            if dst_mac in forward_table.keys():
+                log_debug('packet {} to {}'.format(packet,forward_table.get(dst_mac)[0]))
+                net.send_packet(forward_table.get(dst_mac)[0],packet)
+            else:
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        log_debug ("Flooding packet {} to {}".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)
     net.shutdown()
