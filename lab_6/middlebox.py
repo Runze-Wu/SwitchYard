8f26c5286c755b683e233d9a4864ef30da02b2d1
181860109 <181860109@smail.nju.edu.cn>
2020-05-12 10:48:29 +0800

    lab6 modify middlebox logic

--- lab_6/middlebox.py
+++ lab_6/middlebox.py
@@ -7,16 +7,22 @@ from threading import *
 from random import randint
 import time
 
+
 def switchy_main(net):
 
     my_intf = net.interfaces()
     mymacs = [intf.ethaddr for intf in my_intf]
     myips = [intf.ipaddr for intf in my_intf]
-    ip_mac={"192.168.100.1":"10:00:00:00:00:01","192.168.200.1":"20:00:00:00:00:01"}
+    ip_mac = {
+        "192.168.100.1": "10:00:00:00:00:01",
+        "192.168.200.1": "20:00:00:00:00:01",
+        "middlebox-eth0": "40:00:00:00:00:01",
+        "middlebox-eth1": "40:00:00:00:00:02"
+    }
     while True:
         gotpkt = True
         try:
-            timestamp,dev,pkt = net.recv_packet()
+            timestamp, dev, pkt = net.recv_packet()
             log_debug("Device is {}".format(dev))
         except NoPackets:
             log_debug("No packets available in recv_packet")
@@ -35,19 +41,16 @@ def switchy_main(net):
             Should I drop it?
             If not, modify headers & send to blastee
             '''
-            drop_rate=float(0)
-            middlbox_params=open("middlebox_params", 'r')
-            line=middlbox_params.read().strip().split()
-            if len(line)==2:
-                drop_rate=float(line[1])
-            if randint(0,100)<drop_rate*100: #丢弃
+            drop_rate = float(0)
+            middlbox_params = open("middlebox_params", 'r')
+            line = middlbox_params.read().strip().split()
+            if len(line) == 2:
+                drop_rate = float(line[1])
+            if randint(0, 100) < drop_rate * 100:  #丢弃
                 pass
-            else: #进行发送
-                for intf in my_intf:
-                    if intf.name=="middlebox-eth1":    
-                        pkt[Ethernet].src=intf.ethaddr
-                        break
-                pkt[Ethernet].dst=ip_mac[pkt[IPv4].dst]
+            else:  #进行发送
+                pkt[Ethernet].src=ip_mac[dev]
+                pkt[Ethernet].dst = ip_mac[pkt[IPv4].dst]
                 net.send_packet("middlebox-eth1", pkt)
         elif dev == "middlebox-eth1":
             log_debug("Received from blastee")
@@ -56,11 +59,8 @@ def switchy_main(net):
             Modify headers & send to blaster. Not dropping ACK packets!
             net.send_packet("middlebox-eth0", pkt)
             '''
-            for intf in my_intf:
-                if intf.name=="middlebox-eth0":    
-                    pkt[Ethernet].src=intf.ethaddr
-                    break
-            pkt[Ethernet].dst=ip_mac[pkt[IPv4].dst]
+            pkt[Ethernet].src=ip_mac[dev]
+            pkt[Ethernet].dst = ip_mac[pkt[IPv4].dst]
             net.send_packet("middlebox-eth0", pkt)
         else:
             log_debug("Oops :))")
