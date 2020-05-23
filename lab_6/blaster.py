864d33ede0389d63c7b4f9dd30f185045909a267
181860109 <181860109@smail.nju.edu.cn>
2020-05-23 15:42:37 +0800

    bug fix

--- lab_6/blaster.py
+++ lab_6/blaster.py
@@ -70,13 +70,13 @@ def switchy_main(net):
             if ack_seq in send_list:
                 send_list.remove(ack_seq)
             if ack_seq == LHS:
+                timer = time.time()  #restart the timer
                 if len(send_list) != 0:
                     LHS = sorted(list(send_list))[0]
                 elif len(pkt_fifo) != 0:
                     LHS = pkt_fifo[0]
                 else:
                     LHS = num + 1
-            # LHS = (ack_seq == LHS)
             print("got ack {} LHS: {} RHS: {}".format(ack_seq, LHS, RHS))
         else:
             log_debug("Didn't receive anything")
@@ -84,29 +84,25 @@ def switchy_main(net):
             judge if time delay occur
             '''
             if time.time() - timer >= (timeout) / 1000:
-                print("timeout meet")
+                print("timeout meet {} {}".format(time.time(),timer))
                 timeout_count += 1
-                pkt_fifo.extend(send_list)
+                pkt_fifo.extend(send_list)  #add already send but no ack pkt
                 pkt_fifo = sorted(list(set(pkt_fifo)))
-                timer = time.time()
+                timer = time.time()  #restart the timer
 
-        print("current LHS: {} RHS: {}".format(LHS, RHS))
         if LHS == num + 1:
             '''already done'''
             duration, fullpkt = time.time() - begin_time, sum(pkt_send_count)
-            log_info("total time is {:.3f}".format(duration))
-            log_info("send packet num is {}".format(fullpkt))
-            log_info("Number of coarse timeouts {}".format(timeout_count))
-
-            print("send case {}".format(pkt_send_count[1:]))
-            for item in pkt_send_count:
+            #print("send case {}".format(pkt_send_count[1:]))
+            for item in pkt_send_count[1:]:
                 if item == 1: once_sent += 1
                 else: re_sent += (item - 1)
-            log_info("resent num : {} only once num : {}".format(
-                re_sent, once_sent))
             full, good = fullpkt * length, num * length
-            log_info("Throughput: {:.3f}Bps Goodput: {:.3f}Bps".format(
-                full / duration, good / duration))
+            log_info("Total TX time: {:.3f}s".format(duration))
+            log_info("Number of reTX: {}".format(re_sent))
+            log_info("Number of coarse TOs: {}".format(timeout_count))
+            log_info("Throughput: {:.3f}Bps".format(full / duration))
+            log_info("Goodput: {:.3f}Bps".format(good / duration))
             break
 
         if len(pkt_fifo) == 0: continue
